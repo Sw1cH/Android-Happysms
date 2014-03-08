ac9d385ccf8d6aef079900f8622ad27f78e97214
sw1ch <olegoleg17@gmail.com>
2014-03-08 11:46:26 +0200

    Racist jokes blue button added

--- src/com/sw1ch/happysms/MainActivity.java
+++ src/com/sw1ch/happysms/MainActivity.java
@@ -1,6 +1,7 @@
 package com.sw1ch.happysms;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import android.app.Activity;
 import android.content.res.Resources;
@@ -19,7 +20,8 @@ public class MainActivity extends Activity implements TextWatcher {
 	private static final String TAG = "OnClick";
 	private String phone_number = "28814070";
 	private int x;
-	private String[] messages;
+	private String[] motherMes;
+	private String[] raccismMes;
 	private SmsManager smsMgr;
 	private ArrayList<String> message;
 	private String number_text;
@@ -27,6 +29,27 @@ public class MainActivity extends Activity implements TextWatcher {
 	private EditText number_view;
 	private Button number_button;
 	private TextView numberInfo;
+	private Random index;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		smsMgr = SmsManager.getDefault();
+
+		// get string resources
+		Resources res = getResources();
+		motherMes = res.getStringArray(R.array.mother_message);
+		raccismMes = res.getStringArray(R.array.racist_message);
+		
+		number_view = (EditText) findViewById(R.id.number);
+		number_view.addTextChangedListener(this);
+		number_hint = number_view.getHint().toString();
+		number_button = (Button) findViewById(R.id.change_number);
+		numberInfo = (TextView) findViewById(R.id.info);
+		index = new Random();
+	}
 
 	@Override
 	public void onTextChanged(CharSequence s, int start, int before, int count) {
@@ -68,34 +91,28 @@ public class MainActivity extends Activity implements TextWatcher {
 
 	}
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-
-		smsMgr = SmsManager.getDefault();
-
-		// get string resources
-		Resources res = getResources();
-		messages = res.getStringArray(R.array.message);
-
-		number_view = (EditText) findViewById(R.id.number);
-		number_view.addTextChangedListener(this);
-		number_hint = number_view.getHint().toString();
-		number_button = (Button) findViewById(R.id.change_number);
-		numberInfo = (TextView) findViewById(R.id.info);
-	}
-
 	// OnClick method for send button
-	public void sendSMS(View v) {
-		x = (int) (Math.random() * (messages.length + 1));
-		SmsManager smsMgr = SmsManager.getDefault();
+	public void sendMothSMS(View v) {
+		// Get random x and number from edit text hint
+		x = (int) (Math.abs(index.nextInt()) % motherMes.length);
 		phone_number = number_view.getHint().toString();
-		Log.d(TAG, "got edit_hint: " + number_hint);
-		message = smsMgr.divideMessage(messages[x]);
+
+		message = smsMgr.divideMessage(motherMes[x]);
 		smsMgr.sendMultipartTextMessage(phone_number, null, message, null, null);
 		Toast.makeText(this, "Красава!", Toast.LENGTH_SHORT).show();
-		Log.d(TAG, "sms send to " + phone_number);
+		Log.d(TAG, "Mother joke send to " + phone_number + " " + message);
+	}
+
+	public void sendRacSMS(View v) {
+		// Get random x and number from edit text hint
+		x = (int) (Math.abs(index.nextInt()) % raccismMes.length);
+		phone_number = number_view.getHint().toString();
+		
+		message = smsMgr.divideMessage(raccismMes[x]);
+		smsMgr.sendMultipartTextMessage(phone_number, null, message, null, null);
+		Toast.makeText(this, "Красава!", Toast.LENGTH_SHORT).show();
+		Log.d(TAG, "Racist joke send to " + phone_number + " " + message);
+
 	}
 
 	// OnClick method for save button
